44a7e25955e1ded49bd8d8a2f733e1012f93421d
mohamadsadeque <30416594+mohamadsadeque@users.noreply.github.com>
2017-09-21 09:50:21 -0300

    Update limite.js

--- limite.js
+++ limite.js
@@ -2,29 +2,30 @@ var blocos = new Array();
 blocos[0] =  [1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1];
 blocos[1] =  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
 blocos[2] =  [1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
-blocos[3] =  [1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,1];
-blocos[4] =  [1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
-blocos[5] =  [1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
+blocos[3] =  [1,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,1];
+blocos[4] =  [1,0,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
+blocos[5] =  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
 blocos[6] =  [1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
 blocos[7] =  [1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
 blocos[8] =  [1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
 blocos[9] =  [1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
 blocos[10] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-blocos[11] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
-blocos[12] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
-blocos[13] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
-blocos[14] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
-blocos[15] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
-blocos[16] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
-blocos[17] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
-blocos[18] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1];
+blocos[11] = [1,0,1,0,1,0,1,0,1,1,0,1,1,1,1,0,1,1,1,0,1];
+blocos[12] = [1,0,1,0,1,0,1,0,1,1,0,1,1,1,1,0,1,1,1,0,1];
+blocos[13] = [1,0,1,0,1,0,0,0,1,1,0,1,1,1,1,0,1,1,1,0,1];
+blocos[14] = [1,0,0,0,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,0,1];
+blocos[15] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,0,1];
+blocos[16] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,0,1];
+blocos[17] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,0,1];
+blocos[18] = [1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,0,1];
 blocos[19] = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
 blocos[20] = [1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1];
 
-var x = 210;
-var y = 210;
+var x = 420;
+var y = 420;
+
 function setup() {
-  createCanvas(420, 420);
+  createCanvas(840, 840);
 }
 /*function fase(nivel){
 	switch (nivel)
@@ -32,26 +33,44 @@ function setup() {
    case 1: 
       
        break;
-
    default: 
        alert("Não tem essa fase");
        break;
 }
 	}*/
+	
+	
+function teleport(){
+	 if(x > 815 ){				// TELEPORT
+	   x = 20;
+	   y = 420;}
+	else if(x<15){
+		x = 815;
+		y = 420;
+	}	
+	 else if(y<15){
+		 x = 420;
+		 y = 815;
+	}	
+	  else if(y>815){
+		x = 420;
+	    y = 15;
+	}	}
+	
 
 function colisao(x,y,xr,yr){
-  for(var i=0;i<=20;i++){
-    var newx=(14/2)*cos((i/10)*Math.PI)+x
-    var newy=(14/2)*sin((i/10)*Math.PI)+y
-  	if(xr<=newx&& xr+20>=newx && yr<=newy && yr+20>=newy){
+  for(var i=0;i<=40;i++){
+    var newx=(28/2)*cos((i/10)*Math.PI)+x
+    var newy=(28/2)*sin((i/10)*Math.PI)+y
+  	if(xr<=newx&& xr+40>=newx && yr<=newy && yr+40>=newy){
      console.log(true)
     }
   }
 }
 
 function limite(nx,ny){
-      for(var c = parseInt((nx-8)/20); c <= parseInt((nx+8)/20); c++ ){
-      	for(var l = parseInt((ny-8)/20); l <= parseInt((ny+8)/20); l++ ){
+      for(var c = parseInt((nx-16)/40); c <= parseInt((nx+16)/40); c++ ){
+      	for(var l = parseInt((ny-16)/40); l <= parseInt((ny+16)/40); l++ ){
             if(blocos[l][c]==1){
             	return true
             }
@@ -65,22 +84,27 @@ function draw() {
    for(var j = 0; j <21; j++){
 			if(blocos[j][i] == 1){
               	noStroke();
-              	fill(Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256)); //LOUCURA
-          		rect(i*20,j*20 , 20, 20);
+              	//fill(Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256)); //LOUCURA
+          		rect(i*40,j*40 , 40, 40);
             }
 	}
   }
-	if(keyIsDown(LEFT_ARROW)&&!limite(x-2,y)){
-      x-=2;
-    }else if(keyIsDown(RIGHT_ARROW)&&!limite(x+2,y)){
-      x+=2;
-  	}else if(keyIsDown(UP_ARROW)&&!limite(x,y-2)){
-      y-=2;
-    }else if(keyIsDown(DOWN_ARROW)&&!limite(x,y+2)){
-      y+=2;
+	if(keyIsDown(LEFT_ARROW)&&!limite(x-3,y)){
+		
+      x-=3;
+    }else if(keyIsDown(RIGHT_ARROW)&&!limite(x+3,y)){
+      x+=3;
+  	}else if(keyIsDown(UP_ARROW)&&!limite(x,y-3)){
+      y-=3;
+    }else if(keyIsDown(DOWN_ARROW)&&!limite(x,y+3)){
+      y+=3;
     }
+	teleport();
+	
+	
 	noStroke();
-  	ellipse(x, y, 14, 14);
+  	ellipse(x, y, 28, 28);
+  	
 
  
   
